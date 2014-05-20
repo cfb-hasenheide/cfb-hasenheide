1f63f94d8acc2077be8bc73a3e57524fe7cf8ec9
elgrillo83 <elgrillo83@gmail.com>
2014-05-20 22:14:16 +0200

    prevent self registration

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,15 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  def new
+    flash[:info] = "Aktuell kannst Du dich nicht selbst registrieren, bitte " +
+      "schicke eine E-Mail an 'thilo@cfb-hasenheide.de'"
+    redirect_to root_path
+  end
+
+  def create
+    flash[:info] = "Aktuell kannst Du dich nicht selbst registrieren, bitte " +
+      "schicke eine E-Mail an 'thilo@cfb-hasenheide.de'"
+    redirect_to root_path
+  end
+
+end
