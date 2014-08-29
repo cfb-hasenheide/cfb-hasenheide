505109dddd3a103c15b3c0b68e8b40e450757aa7
elgrillo83 <elgrillo83@gmail.com>
2014-08-29 12:33:17 +0200

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140822185529) do
+ActiveRecord::Schema.define(version: 20140829093742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20140822185529) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "lists", force: true do |t|
+    t.integer  "minimum"
+    t.integer  "maximum"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "event_id"
+  end
+
+  add_index "lists", ["event_id"], name: "index_lists_on_event_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
