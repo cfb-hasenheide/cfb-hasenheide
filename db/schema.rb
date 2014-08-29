1c2bf7b0d66ebc6f7b785e2a607ecc13bc6f2aa7
elgrillo83 <elgrillo83@gmail.com>
2014-08-29 13:23:03 +0200

    add description and address to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140829093742) do
+ActiveRecord::Schema.define(version: 20140829105741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20140829093742) do
     t.datetime "datetime"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "description"
+    t.string   "address"
   end
 
   create_table "lists", force: true do |t|
