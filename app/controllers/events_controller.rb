acf8bdd98e01a14c07036dcf18c92398670c8f3c
elgrillo83 <elgrillo83@gmail.com>
2014-08-29 13:24:02 +0200

    permit nested and new params

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -70,6 +70,10 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:name, :datetime, list: [:minimum, :maximum])
+      params.require(:event).permit(:name,
+                                    :datetime,
+                                    :description,
+                                    :address,
+                                    list_attributes: [:minimum, :maximum])
     end
 end
