318502df02fd19aeb48d107be24db3f5bc92a44e
elgrillo83 <elgrillo83@gmail.com>
2014-08-31 18:38:01 +0200

    list has_and_belongs_to_many users

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,9 +1,9 @@
 class List < ActiveRecord::Base
   belongs_to :event
-  has_many :users, as: :ins
-  has_many :users, as: :outs
-  has_many :users, as: :waitees
-  has_many :users, as: :maybees
+  has_and_belongs_to_many :users, as: :ins
+  has_and_belongs_to_many :users, as: :outs
+  has_and_belongs_to_many :users, as: :waitees
+  has_and_belongs_to_many :users, as: :maybees
 
   validates :minimum, :maximum, presence: true
 end
