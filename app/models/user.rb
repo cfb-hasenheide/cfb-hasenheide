a042582d4102f837192857b44cb14bf66cf74d46
elgrillo83 <elgrillo83@gmail.com>
2014-10-16 21:46:46 +0200

    fix multi named relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :username
+
+  has_and_belongs_to_many :events, foreign_key: :ins
+  has_and_belongs_to_many :events, foreign_key: :outs
+  has_and_belongs_to_many :events, foreign_key: :waitees
+  has_and_belongs_to_many :events, foreign_key: :maybees
 end
