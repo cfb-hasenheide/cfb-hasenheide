876cb124563ce7b8b510ab454cb2355a16d2e402
elgrillo83 <elgrillo83@gmail.com>
2014-10-16 21:47:13 +0200

    create migration for join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140829105741) do
+ActiveRecord::Schema.define(version: 20141016194028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,11 @@ ActiveRecord::Schema.define(version: 20140829105741) do
   end
 
   add_index "lists", ["event_id"], name: "index_lists_on_event_id", using: :btree
+
+  create_table "lists_users", id: false, force: true do |t|
+    t.integer "list_id"
+    t.integer "user_id"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
