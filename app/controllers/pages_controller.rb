002715d06d8dc69c053a4d37f566df35ccfaae38
elgrillo83 <elgrillo83@gmail.com>
2014-11-08 21:42:57 +0100

    add public controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,4 @@
-class PagesController < ApplicationController
+class PagesController < PublicController
   def welcome
   end
 
---
+++ app/controllers/public_controller.rb
@@ -0,0 +1,5 @@
+class PublicController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  layout 'application'
+end
