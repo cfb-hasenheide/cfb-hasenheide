3e9a81c7f095860372044cdf8b2b6c7e289b7bd4
elgrillo83 <elgrillo83@gmail.com>
2014-11-21 22:41:27 +0100

    list attendees

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,16 @@ class Event < ActiveRecord::Base
   has_many :event_responses
 
   validates :name, :datetime, presence: true
+
+  def attendees
+    event_responses.yes.pluck(:user_id).map do |user_id|
+      User.find(user_id)
+    end
+  end
+
+  def maybees
+  end
+
+  def waitess
+  end
 end
