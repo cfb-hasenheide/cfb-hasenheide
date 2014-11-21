fc49b2ea66d9d33d995e54269d9c12d4fef3828f
elgrillo83 <elgrillo83@gmail.com>
2014-11-21 22:40:58 +0100

    re-add missing method

--- app/models/event_response.rb
+++ app/models/event_response.rb
@@ -8,4 +8,8 @@ class EventResponse < ActiveRecord::Base
   validates :user_id, :event_id, :status, presence: true
 
   enum status: %i(yes no maybee watch)
+
+  def self.by_event_and_user(event, user)
+    where(event_id: event, user_id: user).first
+  end
 end
