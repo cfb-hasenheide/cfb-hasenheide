2218ef1feda0c627320ee863b01662f2cd990800
elgrillo83 <elgrillo83@gmail.com>
2014-12-20 22:04:42 +0100

    redirect back after event#update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -51,7 +51,7 @@ class EventsController < ApplicationController
   def update
     respond_to do |format|
       if @event.update(event_params)
-        format.html { redirect_to events_path, notice: 'Event wurde erfolgreich aktualisiert.' }
+        format.html { redirect_to :back, notice: 'Event wurde erfolgreich aktualisiert.' }
         format.json { render :show, status: :ok, location: @event }
       else
         format.html { render :edit }
