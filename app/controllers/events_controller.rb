cfbb5f185a42a663d7f4446817cea7693cffef1c
elgrillo83 <elgrillo83@gmail.com>
2015-01-12 15:53:02 +0100

    refactor EventsController with respond_with

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,13 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
-  # GET /events
-  # GET /events.json
+  respond_to :html
+
   def index
     @upcoming_events = Event.upcoming
     @past_events = Event.past.page(params[:page])
   end
 
-  # GET /events/1
-  # GET /events/1.json
   def show
     @players = User.players
     @yes_replies = Reply.by_event_and_status(@event, :yes)
@@ -19,73 +17,49 @@ class EventsController < ApplicationController
     @watch_replies = Reply.by_event_and_status(@event, :watch)
     @no_reply_players = User.didnt_reply_to_event(@event.id)
     @reply = Reply.for_event_and_user(@event, current_user)
+
+    respond_with(@event)
   end
 
-  # GET /events/new
   def new
     @event = Event.new
+    respond_with(@event)
   end
 
-  # GET /events/1/edit
   def edit
   end
 
-  # POST /events
-  # POST /events.json
   def create
     @event = Event.new(event_params)
+    flash[:notice] = 'Event wurde erfolgreich erstellt.' if @event.save
 
-    respond_to do |format|
-      if @event.save
-        format.html { redirect_to events_path, notice: 'Event wurde erfolgreich erstellt.' }
-        format.json { render :show, status: :created, location: @event }
-      else
-        format.html { render :new }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
-    end
+    respond_with(@event, events_path)
   end
 
-  # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
   def update
-    respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to :back, notice: 'Event wurde erfolgreich aktualisiert.' }
-        format.json { render :show, status: :ok, location: @event }
-      else
-        format.html { render :edit }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
+    if @event.update(event_params)
+      flash[:notice] = 'Event wurde erfolgreich aktualisiert.'
     end
+
+    respond_with(@event)
   end
 
-  # DELETE /events/1
-  # DELETE /events/1.json
   def destroy
     @event.destroy
-    respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event wurde erfolgreich gelöscht.' }
-      format.json { head :no_content }
-    end
+    flash[:notice] = 'Event wurde erfolgreich gelöscht.'
+
+    respond_with(@event)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = Event.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def event_params
-      params.require(:event).permit(:kind,
-                                    :name,
-                                    :datetime,
-                                    :home_team,
-                                    :away_team,
-                                    :description,
-                                    :address,
-                                    :minimum,
-                                    :maximum)
-    end
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:kind, :name, :datetime, :home_team,
+                                  :away_team, :description, :address,
+                                  :minimum, :maximum)
+  end
 end
