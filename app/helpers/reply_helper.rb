eae9fa265ea6659cad8d9098edfbc299bae480aa
elgrillo83 <elgrillo83@gmail.com>
2015-01-12 17:51:44 +0100

    show waiting list position

--- app/helpers/reply_helper.rb
+++ app/helpers/reply_helper.rb
@@ -10,4 +10,14 @@ module ReplyHelper
   def reply_for(event, user)
     Reply.for_event_and_user(event, user)
   end
+
+  # TODO: Please refactor me, I'm so ugly!
+  def waiting_position_for(event, user)
+    waiting_user_ids = Reply.where(event_id: event).waiting
+      .order('updated_at').pluck(:user_id)
+
+    position = waiting_user_ids.index(user.id)
+    return nil unless position
+    position + 1
+  end
 end
