bc7196b59766a9932c4265c2b8342c7fcb077019
elgrillo83 <elgrillo83@gmail.com>
2016-01-27 22:43:25 +0100

    change dummy to of EventMailer#open_replies

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -2,7 +2,7 @@ class EventMailer < ApplicationMailer
   def open_replies(event_id)
     @event = Event.find(event_id)
 
-    mail(to: 'thilo@cfb-hasenheide.de',
+    mail(to: 'to@example.com',
          subject: 'Neuer Termin in der Meldeliste freigegeben')
   end
 end
