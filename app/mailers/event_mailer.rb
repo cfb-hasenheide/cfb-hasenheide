c2d2664feb8054de5cb4246b98b3b8b4b86ce13d
elgrillo83 <elgrillo83@gmail.com>
2016-01-28 22:31:18 +0100

    add EventMailer#close_replies action and views

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -5,4 +5,11 @@ class EventMailer < ApplicationMailer
     mail(to: 'to@example.com',
          subject: 'Neuer Termin in der Meldeliste freigegeben')
   end
+
+  def close_replies(event_id)
+    @event = Event.find(event_id)
+
+    mail(to: 'to@example.com',
+         subject: "Final Call - #{@event.name} (#{@event.model_name.human})")
+  end
 end
