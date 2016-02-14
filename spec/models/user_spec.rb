d680f98f6c20ea0162bfa2d3fa310b15170f09d2
elgrillo83 <elgrillo83@gmail.com>
2016-02-14 21:43:10 +0100

    spec that user_profile gets created/destroyed for user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe User do
+  context 'after create' do
+    it "creates its UserProfile" do
+      expect{ create :user }.to change{ UserProfile.count }.from(0).to(1)
+    end
+  end
+
+  context 'after destroy' do
+    let!(:user) { create :user }
+
+    it "destroys its UserProfile" do
+      expect{ user.destroy }.to change{ UserProfile.count }.from(1).to(0)
+    end
+  end
+end
