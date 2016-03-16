a7182e11d2f3247f8c162e53fcbd30b33b9e1b22
Dennis Haegler <dennis.haegler@gmail.com>
2016-03-16 13:08:43 -0400

    Fix seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
 %w(fan player admin).each do |role|
-  User.send(role).create(username: role,
-                         email:    "#{role}@cfb.de",
-                         password: 'password')
+  User.create(username: role, email: "#{role}@cfb.de", password: 'password')
 end
