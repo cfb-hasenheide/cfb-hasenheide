81253fb131d01334ba51b5da18ac65b18a9bf593
Dennis Haegler <dennis.haegler@gmail.com>
2016-03-19 22:25:40 -0400

    Cleanup schema

--- db/schema.rb
+++ db/schema.rb
@@ -17,20 +17,22 @@ ActiveRecord::Schema.define(version: 20160318102615) do
   enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
+    t.datetime "datetime",                      null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "description",   limit: 255
-    t.string   "address",       limit: 255,                 null: false
-    t.integer  "minimum",                                   null: false
-    t.integer  "maximum",                                   null: false
-    t.string   "name",                                      null: false
+    t.string   "description"
+    t.string   "address",                       null: false
+    t.integer  "minimum",                       null: false
+    t.integer  "maximum",                       null: false
+    t.string   "home_team"
+    t.string   "away_team"
+    t.string   "name",                          null: false
     t.integer  "club_team_id"
     t.integer  "rival_team_id"
-    t.boolean  "home",                                      null: false
-    t.datetime "datetime",                                  null: false
-    t.boolean  "replyable",                 default: false, null: false
-    t.string   "type",                                      null: false
-    t.string   "slug",                                      null: false
+    t.boolean  "home",                          null: false
+    t.boolean  "replyable",     default: false, null: false
+    t.string   "type",                          null: false
+    t.string   "slug",                          null: false
   end
 
   add_index "events", ["slug"], name: "index_events_on_slug", unique: true, using: :btree
@@ -113,31 +115,31 @@ ActiveRecord::Schema.define(version: 20160318102615) do
   add_index "user_profiles", ["user_id"], name: "index_user_profiles_on_user_id", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "",    null: false
-    t.string   "encrypted_password",     limit: 255, default: ""
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: ""
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "username",               limit: 255
-    t.string   "invitation_token",       limit: 255
+    t.string   "username"
+    t.string   "invitation_token"
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
     t.datetime "invitation_accepted_at"
     t.integer  "invitation_limit"
     t.integer  "invited_by_id"
-    t.string   "invited_by_type",        limit: 255
-    t.integer  "invitations_count",                  default: 0
+    t.string   "invited_by_type"
+    t.integer  "invitations_count",      default: 0
     t.boolean  "legacy_password"
-    t.boolean  "admin",                              default: false, null: false
-    t.boolean  "player",                             default: false, null: false
-    t.boolean  "player_pass",                        default: false, null: false
+    t.boolean  "admin",                  default: false, null: false
+    t.boolean  "player",                 default: false, null: false
+    t.boolean  "player_pass",            default: false, null: false
   end
 
   add_index "users", ["admin"], name: "index_users_on_admin", using: :btree
