21362ee691f01d354787771b467201aa316da758
elgrillo83 <elgrillo83@gmail.com>
2016-03-21 22:35:09 +0100

    change email in flash message
    which is shown to people who want to self register

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
   def new
     flash[:info] = 'Aktuell kannst Du dich nicht selbst registrieren, bitte ' \
-      "schicke eine E-Mail an 'thilo@cfb-hasenheide.de'"
+      "schicke eine E-Mail an 'info@cfb-hasenheide.de'"
     redirect_to root_path
   end
 
   def create
     flash[:info] = 'Aktuell kannst Du dich nicht selbst registrieren, bitte ' \
-      "schicke eine E-Mail an 'thilo@cfb-hasenheide.de'"
+      "schicke eine E-Mail an 'info@cfb-hasenheide.de'"
     redirect_to root_path
   end
 end
