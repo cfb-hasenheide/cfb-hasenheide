a9400966c104a2b395cf69bae03d7a3150fd3590
elgrillo83 <elgrillo83@gmail.com>
2016-05-18 21:48:26 +0200

    unset path_style in carrierwave fog_credentials config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,8 +11,7 @@ CarrierWave.configure do |config|
       provider:              'AWS',
       aws_access_key_id:     Rails.application.secrets.aws_access_key_id,
       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-      region:                Rails.application.secrets.aws_region,
-      path_style:            true
+      region:                Rails.application.secrets.aws_region
     }
 
     config.fog_directory  = Rails.application.secrets.aws_bucket
