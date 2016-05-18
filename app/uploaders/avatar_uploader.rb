111f9b6932346a201b29d426522da406304311d4
elgrillo83 <elgrillo83@gmail.com>
2016-05-18 20:00:01 +0200

    provide a default Avatar image

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -15,7 +15,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
     ActionController::Base.helpers.asset_path(
-      'fallback/' + [version_name, 'default.png'].compact.join('_')
+      'images/defaults/' + [version_name, 'avatar.png'].compact.join('_')
     )
   end
 
