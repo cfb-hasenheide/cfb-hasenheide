af666a54e6ac2f52c487467838bc43d1f711b21d
elgrillo83 <elgrillo83@gmail.com>
2016-06-13 22:58:20 +0200

    display 5 (instead of 3) news on welcome page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < PublicController
   def welcome
     @news = News.all
     @news = @news.where(internal: false) unless user_signed_in?
-    @news = @news.order('created_at DESC').limit(3)
+    @news = @news.order('created_at DESC').limit(5)
     @past_events = Event.past(3)
     @future_events = Event.future(3)
   end
