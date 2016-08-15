124818b93759d32cafbd847c3224b99c69916e5d
Dennis Haegler <dennis.haegler@gmail.com>
2016-08-15 19:16:08 -0400

    Add method delegations

--- app/models/function.rb
+++ app/models/function.rb
@@ -2,6 +2,9 @@ class Function < ApplicationRecord
   belongs_to :user
   belongs_to :role
 
+  delegate :username, to: :user
+  delegate :name, to: :role
+
   def self.for(user, role)
     create(user_id: user.id, role_id: role.id)
   end
