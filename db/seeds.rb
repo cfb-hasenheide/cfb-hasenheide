55afb16335f2a74859c732d192d2486c892ee5fb
Dennis Haegler <dennis.haegler@gmail.com>
2016-08-17 14:09:32 -0400

    Add seeds for development database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,23 @@
-%w(fan player admin).each do |role|
-  admin = (role == 'admin')
-  User.create(
-    username: role,
-    email: "#{role}@cfb.de",
-    password: 'password',
-    admin: admin
-  )
+if Rails.env.development?
+  %w(fan player admin).each do |role|
+    admin = (role == 'admin')
+    user = User.create(
+      username: role,
+      email: "#{role}@cfb.de",
+      password: 'password',
+      admin: admin
+    )
+
+    Address.create(
+      street: 'Hasenheide',
+      zipcode: '13346',
+      city: 'Hasentown',
+      addressable: user
+    )
+
+    Player.create(
+      nickname: "Nicki #{role}",
+      user: user
+    )
+  end
 end
