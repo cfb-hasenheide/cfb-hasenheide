0e2eb08602beda84653b5960408e5ca7b9ec5090
Dennis Haegler <dennis.haegler@gmail.com>
2016-08-20 13:08:22 -0400

    Add more validation for function
    Functions for the club cannot be exist twice
    for the same user and role during same time.

--- app/models/function.rb
+++ app/models/function.rb
@@ -2,6 +2,9 @@ class Function < ApplicationRecord
   belongs_to :user
   belongs_to :role
 
+  before_save :correct_time_order?
+  before_save :check_overlapping_time
+
   delegate :username, to: :user
   delegate :name, to: :role
 
@@ -12,5 +15,20 @@ class Function < ApplicationRecord
   def self.current
     where('vacated_at > ? OR vacated_at is NULL AND assumed_at <= ?',
           DateTime.now, DateTime.now)
+  end
+
+  private
+
+  def correct_time_order?
+    return true unless vacated_at
+    throw(:abort) if vacated_at < assumed_at
+  end
+
+  def check_overlapping_time
+    same_functions = Function.where(role: role.id, user: user)
+      .where('vacated_at > ?', assumed_at)
+      .where('assumed_at < ?', vacated_at)
+      .where.not(id: id)
+    throw(:abort) if same_functions.any?
   end
 end
--- spec/models/function_spec.rb
+++ spec/models/function_spec.rb
@@ -3,23 +3,10 @@ require 'rails_helper'
 RSpec.describe Function, type: :model do
   let(:role) { create :role }
   let(:user) { create :user }
-
-  context 'roles assignable to users' do
-    it 'assigns the role to the user' do
-      expect do
-        user.roles << role
-        user.save
-      end.to change { user.functions.count }.from(0).to(1)
-    end
+  let(:function) do
+    create :function, :current_function, role: role, user: user
   end
 
-  describe '.create' do
-    it 'assigns roles to user' do
-      expect do
-        Function.create(user_id: user.id, role_id: role.id)
-      end.to change { user.functions.count }.from(0).to(1)
-    end
-  end
 
   describe '.for' do
     it 'assigns roles to user' do
@@ -29,20 +16,17 @@ RSpec.describe Function, type: :model do
     end
   end
 
-  context 'delegating methods to relations functions' do
-    before do
-      user.assign_role role
-    end
 
+  context 'delegating methods to relations functions' do
     describe '#name' do
       it 'returns the name of the role' do
-        expect(user.functions.first.name).to eq(role.name)
+        expect(function.name).to eq(role.name)
       end
     end
 
     describe '#username' do
       it 'returns the name of the user' do
-        expect(user.functions.first.username).to eq(user.username)
+        expect(function.username).to eq(user.username)
       end
     end
   end
@@ -75,6 +59,83 @@ RSpec.describe Function, type: :model do
     it 'returns only functions started in the past' do
       Function.current.each do |function|
         expect(function.assumed_at).to be < Date.today + 1
+      end
+    end
+  end
+
+  context 'validate assumed_at and vacated_at' do
+    let(:assumed_at) { DateTime.now }
+    let(:new_function) do
+      build :function, assumed_at: assumed_at, vacated_at: vacated_at
+    end
+
+    context 'assumed_at > vacated_at' do
+      let(:vacated_at) { assumed_at - 10 }
+
+      it 'cannot persist the function' do
+        expect(new_function.save).to be_falsy
+      end
+    end
+
+    context 'assumed_at < vacated_at' do
+      let(:vacated_at) { assumed_at + 10 }
+
+      it 'persists the function' do
+        expect(new_function.save).to be_truthy
+      end
+    end
+  end
+
+  describe '#update' do
+    let(:updating_function) do
+      create :function, role: function.role, user: function.user,
+        assumed_at: function.vacated_at + 10, vacated_at: function.vacated_at + 20
+    end
+
+    subject { updating_function.update(vacated_at: time_for_update) }
+
+    context 'no times overlapping with existing function' do
+      let(:time_for_update) { updating_function.vacated_at + 2 }
+
+      it 'updates the function' do
+        expect(subject).to be_truthy
+      end
+    end
+
+    context 'times will be overlapping with existing existing function' do
+      let(:time_for_update) { function.vacated_at - 2 }
+
+      it 'cannot update the function' do
+        expect(subject).to be_falsy
+      end
+    end
+  end
+
+  context 'same user and role' do
+    let(:new_function) do
+      build :function,
+        user: user,
+        role: role,
+        assumed_at: assumed_at,
+        vacated_at: vacated_at
+    end
+
+    context 'during the existing time of other function' do
+      let(:assumed_at) { function.assumed_at }
+      let(:vacated_at) { function.vacated_at + 1 }
+
+      it 'cannot be created' do
+        expect(new_function.save).to be_falsy
+      end
+
+    end
+
+    context 'different time line' do
+      let(:assumed_at) { function.vacated_at }
+      let(:vacated_at) { function.vacated_at + 100 }
+
+      it 'can be created' do
+        expect(new_function.save).to be_truthy
       end
     end
   end
