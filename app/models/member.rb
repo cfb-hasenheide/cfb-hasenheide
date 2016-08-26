ed89d0f32f857ccf824a502a91ade04613c43ffc
Dennis Haegler <dennis.haegler@gmail.com>
2016-08-26 18:29:26 -0400

    Add member edit form

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,4 @@
 class Member < ApplicationRecord
   belongs_to :user
+  delegate :player, to: :user
 end
---
+++ spec/controllers/members_controller_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe MembersController do
+  let(:member) { create(:member) }
+  before { sign_in }
+
+  describe '#edit' do
+    it 'renders edit form' do
+      get :edit, params: { id: member.id }
+      expect(response).to render_template(:edit)
+    end
+  end
+
+  describe '#update' do
+    let(:identifier) { '123456789' }
+    subject do
+      put :update, params: { id: member.id, member: { identifier: identifier } }
+    end
+
+    it 'updates the identifier' do
+      expect do
+        subject
+      end.to change { member.reload.identifier }.to(identifier)
+    end
+
+    it "redirects to player/:id" do
+      expect(subject).to redirect_to(player_path(member.player))
+    end
+  end
+end
