405757a256262bbac4e2f00b3f2cddb3bb925e4f
Dennis Haegler <dennis.haegler@gmail.com>
2016-09-12 18:07:47 -0400

    Update schema

--- db/migrate/20160909024812_create_join_table_players_teams.rb
+++ db/migrate/20160909024812_create_join_table_players_teams.rb
@@ -2,7 +2,7 @@ class CreateJoinTablePlayersTeams < ActiveRecord::Migration[5.0]
   def change
     create_join_table :players, :teams do |t|
       t.index [:team_id, :player_id], unique: true
-      t.index [:player_id, :player_id], unique: true
+      t.index [:player_id, :team_id], unique: true
     end
   end
 end
