8d770c49f38e932e531d9aa11fefb20cd54d031d
Thilo Grashei <thilo.grashei@kaeuferportal.de>
2017-07-03 22:00:47 +0200

    Filter goals by given event

--- app/controllers/match_events_controller.rb
+++ app/controllers/match_events_controller.rb
@@ -1,6 +1,6 @@
 class MatchEventsController < ApplicationController
   def index
     @event = Event.friendly.find(params[:event_id])
-    @goals = Goal.all.order(:minute, :extra_time)
+    @goals = @event.goals.order(:minute, :extra_time)
   end
 end
