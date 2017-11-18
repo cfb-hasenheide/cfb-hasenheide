de400eda814ce31d8d9b20b7f6956c8cf8b4a9b5
Thilo <thilo@cfb-hasenheide.de>
2017-11-18 21:59:34 +0100

    Define through association before using it
    Occured after upgrading to Rails 5.1, see: https://github.com/rails/rails/issues/29123

--- app/models/events/event.rb
+++ app/models/events/event.rb
@@ -4,9 +4,9 @@ class Event < ApplicationRecord
   paginates_per 12
 
   belongs_to :club_team, class_name: 'Team'
+  has_one :attendance_list, as: :attendable, dependent: :destroy
   has_many :attendances, through: :attendance_list
   has_many :goals
-  has_one :attendance_list, as: :attendable, dependent: :destroy
   has_one :report, dependent: :destroy
 
   after_initialize :set_public, unless: :persisted?
