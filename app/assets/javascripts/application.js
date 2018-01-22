536b0ed4f713ed0b557bfade3632c19ebfcdf762
elgrillo83 <elgrillo83@gmail.com>
2018-01-22 20:22:09 +0200

    Update bootstrap gem and use assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery3
 //= require jquery_ujs
 //= require ckeditor/init
+//= require popper
 //= require bootstrap
 //= require bootstrap-switch
 //= require Chart.bundle
